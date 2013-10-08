a5f37aed15c9cd0e06fdd7c33cbc58584f86230c
Greyson Parrelli <greyson.parrelli@live.com>
2013-10-08 10:20:39 -0400

    Wrote basic class sample.

--- samples/6_classes/script.js
+++ samples/6_classes/script.js
@@ -1,6 +1,43 @@
 /**
- * JavaScript doesn't have a 'classical' inheritance structure. It uses
- * what is called 'prototypal' inheritance. However, we can mimic a lot of
- * the classical model with the prototypal model.
+ * Using a regular object to mimic a class in JavaScript poses some problems.
+ * There aren't really any immediately obvious ways to have a transparent
+ * constructor and private variables. Instead of using objects, we use
+ * functions, which can be treated like objects themselves. Here we'll 
+ * go through an example.
  */
 
+// The function signature can be considered the constructor
+var Person = function(name, age) {
+    // The 'this' keyword refers to the public properties of this
+    // function. 
+    this.name = name;
+    this.age = age;
+
+    // Variables declared in a function have their scope limited
+    // to that function, so this variable is essentially private
+    var soul = 42;
+
+    // Here we're adding some more public properties, this time in the
+    // form of a function. 
+    this.speak = function() {
+        console.log("My name is " + this.name);
+    }
+    this.showSoul = function() {
+        console.log("My soul looks like this: " + soul);
+    }
+
+    // Here's an example of a private function
+    function invisible() {
+        console.log("You can only access me from inside here.");
+    }
+}
+
+var peter = new Person("Peter Parker", 15);
+var maryjane = new Person("Mary Jane", 15);
+
+peter.speak();
+maryjane.speak();
+
+peter.showSoul();
+peter.soul = 5;
+peter.showSoul();
